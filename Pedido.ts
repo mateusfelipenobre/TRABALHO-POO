61bafe286b5d98211f0663cb9c918fa2d35bdfc7
mateusfelipenobre <42943955+mateusfelipenobre@users.noreply.github.com>
2018-12-02 22:03:52 -0200

    Add files via upload

--- Pedido.ts
+++ Pedido.ts
@@ -1,7 +1,19 @@
 class Pedido{
-    private Pedinte : Cliente[];
+    private pedinte : Cliente[];
     private pacote : Produto[];
     private tempoEntreg : number;
     private valorFinal : number;
 
+    public constructor(pedinte : Cliente[], pacote : Produto[], tempoEntreg : number, valorFinal : number){
+        this.pedinte = pedinte;
+        this.pacote = pacote;
+        this.tempoEntreg = tempoEntreg;
+        this.valorFinal = valorFinal;
+    }
+
+    public tempoTotal(produto : Produto){
+        let total : number;
+        total = produto.getTempoPrep() + this.tempoEntreg;
+        return total; 
+    }
 }
